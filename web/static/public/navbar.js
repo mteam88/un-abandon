f8519b81934bb93ce99505d79648f8ccd802cd95
Matthew  Edelen <84196639+mteam88@users.noreply.github.com>
2022-10-30 20:30:24 +0000

    convert to gh-pages

--- web/static/public/navbar.js
+++
@@ -1,6 +0,0 @@
-function dynamicallySelectActiveTab() {
-  var path = window.location.pathname;
-  var activeTab = document.querySelector('a[href="' + path + '"]');
-  activeTab.classList.add('active');
-}
-dynamicallySelectActiveTab();
