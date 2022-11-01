8a843ae2a7aa3ffee83ca33fc8fd814b0bc409ac
Matthew  Edelen <84196639+mteam88@users.noreply.github.com>
2022-11-01 01:40:06 +0000

    created basic view

--- app/dashboard.go
+++ app/dashboard.go
@@ -7,6 +7,12 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Repo struct {
+		Name        string `json:"name"`
+		Description string `json:"description"`
+		Url         string `json:"html_url"`
+}
+
 func DashboardSetup() {
 	App.Get("/dashboard", func(c *fiber.Ctx) error {
 		ctx := context.Background()
@@ -18,10 +24,19 @@ func DashboardSetup() {
 			log.Print(err)
 			return err
 		}
+		// clean repos object to only include name, url and description
+		var cleanRepos []Repo = []Repo{}
+		for _, repo := range repos {
+			cleanRepos = append(cleanRepos, Repo{
+				Name:        repo.GetName(),
+				Description: repo.GetDescription(),
+				Url:         repo.GetHTMLURL(),
+			})
+		}
 
 		return c.Render("dashboard", fiber.Map{
 			"Header": "Dashboard",
-			"Repos": repos,
+			"Repos": cleanRepos,
 		}, "layouts/main")
 	})
 }
