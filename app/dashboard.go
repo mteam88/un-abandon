ec24ddaec06b1f316830ce54996db5e30f0a4b71
Matthew  Edelen <84196639+mteam88@users.noreply.github.com>
2022-11-02 11:41:13 +0000

    basic transfer added. NOT TESTED

--- app/dashboard.go
+++ app/dashboard.go
@@ -8,6 +8,7 @@ import (
 	"github.com/mteam88/un-abandon/database"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/go-github/v48/github"
 )
 
 func DashboardSetup() {
@@ -102,4 +103,95 @@ func DashboardSetup() {
 		return c.Redirect("/dashboard")
 	})
 
+	App.Post("/dashboard/adopt/", func(c *fiber.Ctx) error {
+		ctx := context.Background()
+		client := GetGithubClient(ctx, c.Cookies("github_token"))
+
+		var url struct {
+			Url string `json:"url"`
+		}
+
+		err := json.Unmarshal(c.Body(), url)
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+
+		abandoned_repos, err := DB.Get("abandoned_repos")
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+		var repos []database.Repo
+		err = json.Unmarshal(abandoned_repos, &repos)
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+
+		for i, repo := range repos {
+			if repo.Url == url.Url {
+				// remove repo from db
+				repos = append(repos[:i], repos[i+1:]...)
+				abandoned_repos, err = json.Marshal(repos)
+				if err != nil {
+					log.Print(err)
+					return err
+				}
+				err = DB.Set("abandoned_repos", abandoned_repos)
+				if err != nil {
+					log.Print(err)
+					return err
+				}
+				
+				err = TransferRepo(repo, client)
+				if err != nil {
+					log.Print(err)
+					return err
+				}
+			}
+		}
+
+		return c.Redirect("/dashboard")
+	})
+}
+
+
+func TransferRepo(dbrepo database.Repo, newOwnerClient *github.Client) error {
+	ctx := context.Background()
+	currentOwnerClient := GetGithubClient(ctx, dbrepo.Token)
+
+	// get repos
+	ghrepos, _, err := currentOwnerClient.Repositories.ListAll(ctx, nil)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	// find repo that matches dbrepo
+	var ghrepo *github.Repository
+	for _, repo := range ghrepos {
+		if repo.GetID() == dbrepo.ID {
+			ghrepo = repo
+		}
+	}
+
+	// get new client username
+	newOwner, _, err := newOwnerClient.Users.Get(ctx, "")
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	newOwnerUsername := newOwner.GetLogin()
+
+	// transfer repo
+	_, _, err = currentOwnerClient.Repositories.Transfer(ctx, ghrepo.GetOwner().GetLogin(), ghrepo.GetName(), github.TransferRequest{
+		NewOwner: newOwnerUsername,
+	})
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	return nil
 }
