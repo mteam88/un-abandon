f809b134242cbc2a01621b9cf208ce50a64721ac
Matthew  Edelen <84196639+mteam88@users.noreply.github.com>
2022-11-04 15:41:27 +0000

    explore view working

--- app/explore.go
+++ app/explore.go
@@ -9,6 +9,32 @@ func ExploreSetup() {
 	ExploreGroup := App.Group("/explore")
 
 	ExploreGroup.Get("/", func(c *fiber.Ctx) error {
+		ctx := context.Background()
+		client := GetGithubClient(ctx, c.Cookies("github_token"))
+
+		repos, _, err := client.Repositories.List(ctx, "", nil)
+
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+		// clean repos object to only include name, url and description
+		var cleanRepos []database.Repo = []database.Repo{}
+		for _, repo := range repos {
+			cleanRepos = append(cleanRepos, database.Repo{
+				Name:        repo.GetName(),
+				Description: repo.GetDescription(),
+				Url:         repo.GetHTMLURL(),
+				ID:          repo.GetID(),
+			})
+		}
+
+		user, _, err := client.Users.Get(ctx, "")
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+
 		return c.Render("explore", fiber.Map{
 			"Header": "Explore",
 		}, "layouts/main")
