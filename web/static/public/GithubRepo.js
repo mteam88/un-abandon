510ccbbc1a0d9cc9b1c30807f0f986f8c0e8b97a
Matthew  Edelen <84196639+mteam88@users.noreply.github.com>
2022-11-08 02:48:48 +0000

    fix url on cards

--- web/static/public/GithubRepo.js
+++ web/static/public/GithubRepo.js
@@ -6,7 +6,7 @@ export default {
     template: `
     <div class="card" style="column-break-inside: avoid;">
     <div class="card-body">
-      <h5 class="card-title"><a :href="repo.url" target="_blank">{{ repo.name }}</a></h5>
+      <h5 class="card-title"><a :href="repo.html_url" target="_blank">{{ repo.name }}</a></h5>
       <p class="card-text">{{ repo.description }}</p>
       <a class="btn btn-danger" @click="$emit('button-clicked')" >{{ buttontext }}</a>
     </div>
